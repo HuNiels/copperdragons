7431700ab286f7678a8a75eaac31fda5cff428d6
Niels <nielsvanrijn.nvr@gmail.com>
2026-04-10 15:02:03 +0200

    Refactored spells. Can now use an argument from the command line to define a spell to be 'cast', aslong as it is available in valid_spells

--- rpi-rgb-led-matrix/bindings/python/cde-led-dev/spells/spell.py
+++ rpi-rgb-led-matrix/bindings/python/cde-led-dev/spells/spell.py
@@ -2,7 +2,6 @@
 import sys
 import os
 import time
-import re
 
 from pathlib import Path
 import json
@@ -11,56 +10,53 @@ sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/..'))
 
 #Define what spell to 'cast', lowercase
 valid_spells = ["fireball", "void"]
-spell_selected = ""
+selected_spell = ""
 
 arguments = sys.argv
 
 for argument in arguments:
     if argument in valid_spells:
         index = arguments.index(argument)
-        spell_selected = arguments.pop(index)
+        selected_spell = arguments.pop(index)
         break
 
 sys.argv = arguments
 
+if not selected_spell:
+    print(f"No spell selected, please select one of the following spells: {valid_spells}")
+    sys.exit()
+
 from samplebase import SampleBase
 
-FRAME_FOLDER = Path("../pixel-data")
+FRAME_DATA_FOLDER = Path(f"../pixel-data/{selected_spell}")
+FRAME_TIME = 0.1
+
+GRID = "grid"
+PALETTE = "palette"
+
+FRAMES = max(
+    int(f.stem.split('_')[-1])
+    for f in FRAME_DATA_FOLDER.glob(f"{selected_spell}-frame_*.json")
+)
 
 class Spell(SampleBase):
     def __init__(self, *args, **kwargs):
         super(Spell, self).__init__(*args, **kwargs)
 
     def run(self):
-
-        if not spell_selected:
-            print("No spell selected.")
-            sys.exit()
-
-        if spell_selected not in valid_spells:
-            print("Selected spell not in spellbook, please try another.")
-            sys.exit()
-
-        FRAMES = max(
-            int(re.search(r"\d+", f.stem).group())
-            for f in FRAME_FOLDER.glob(f"{spell_selected}-frame_*.json")
-        )
-
-        while(1):
+        while(True):
+            with open(f"{FRAME_DATA_FOLDER}/{selected_spell}-colours.json", "r") as f:
+                spell_matrix_colours = json.load(f)
             for frame in range(FRAMES):
-                with open(f"{FRAME_FOLDER}/{spell_selected}-frame_{frame}.json", "r") as f:
+                with open(f"{FRAME_DATA_FOLDER}/{selected_spell}-frame_{frame}.json", "r") as f:
                     spell_matrix = json.load(f)
-
-                with open(f"{FRAME_FOLDER}/{spell_selected}-colours.json", "r") as f:
-                    spell_matrix_colours = json.load(f)
 
                 offset_canvas = self.matrix.CreateFrameCanvas()
 
-                # while True:
-                for x in range(0, len(spell_matrix["grid"][0])):
-                    for y in range(0, len(spell_matrix["grid"])):
-                        colour_index = spell_matrix["grid"][y][x]
-                        colour = spell_matrix_colours["palette"][colour_index]
+                for x in range(0, len(spell_matrix[GRID][0])):
+                    for y in range(0, len(spell_matrix[GRID])):
+                        colour_index = spell_matrix[GRID][y][x]
+                        colour = spell_matrix_colours[PALETTE][colour_index]
                         r = colour["r"]
                         g = colour["g"]
                         b = colour["b"]
@@ -68,7 +64,7 @@ class Spell(SampleBase):
 
                 self.matrix.SwapOnVSync(offset_canvas)
 
-                time.sleep(0.1)
+                time.sleep(FRAME_TIME)
     
 # Main function
 if __name__ == "__main__":
